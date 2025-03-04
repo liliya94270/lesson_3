944eb64a6e6d9d962baacd981a2ac8807b0095c3
liliya94270 <liya.khabibullina.10@mail.ru>
2025-03-04 13:40:45 +0500

    Add files via upload

---
+++ Lesson_3_task_3.py
@@ -0,0 +1,15 @@
+from address import Address
+from mailing import Mailing
+
+to_address = Address(index: "450000", city: "Уфа", street: "Цюрупа", house: "84", apartment: "6")
+from_addrees =Address(index: "450001", city: "Янаул", street: "Победы", house: "101", apartment: "3")
+mailing =Mailing(to_adress:"1247890h", from_addrees,  to_address track:"17" )
+
+print(mailing)
+
+
+#Распечатайте в консоль отправление в формате:
+# Отправление <track>
+#  из <индекс>, <город>, <улица>, <дом> -<квартира>
+# в <индекс>, <город>, <улица>, <дом>-<квартира>.
+# Стоимость <стоимость> рублей.
