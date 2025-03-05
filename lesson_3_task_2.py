30c42ebeb0778bf3316aac557fa111df6a8b708e
liliya94270 <liya.khabibullina.10@mail.ru>
2025-03-05 13:59:05 +0500

    Update lesson_3_task_2.py

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -2,12 +2,12 @@ from smartphone import Smartphone
 
 
 catalog = []
-catalog.append(Smartphone("Honor", "50","8996-153-430-33"))
-catalog.append(Smartphone("Huawei", "P20", "8995-123-36-37"))
-catalog.append(Smartphone("Iphone", "XS", "8965-456-65-44"))
-catalog.append(Smartphone("Samsung", "S20", "8994-567-74-46"))
-catalog.append(Smartphone("Realme", "c61", "8995-243-33-67"))
+catalog.append(Smartphone(<Honor> - <50>. <+7996-153-430-33>.))
+catalog.append(Smartphone(<Huawei> - <P20>. <+7995-123-36-37>.))
+catalog.append(Smartphone(<Iphone> -  <XS>. <+7965-456-65-44>.))
+catalog.append(Smartphone(<Samsung> - <S20>. <+7994-567-74-46>.))
+catalog.append(Smartphone(<Realme> - <c61>. <+7995-243-33-67>.))
 
 
 for phone in catalog:
-   print(f'{phone.phone_brand}, {phone.phone_model}, {phone.number}')
+   print(f'{phone.brand}, {phone.model}, {phone.number}')
