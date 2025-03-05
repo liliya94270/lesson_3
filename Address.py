9618d7ba7f921ec0f397308e6d0a7f5262c07c31
liliya94270 <liya.khabibullina.10@mail.ru>
2025-03-05 14:02:40 +0500

    Update and rename Adress.py to Address.py

--- Address.py
+++ Address.py
@@ -1,5 +1,5 @@
 class Address:
-def __init__(self, index, city, street, house, apartment):
+def __init__ (self, index, city, street, house, apartment):
     self.index = index
     self.city = city
     self.street = street
@@ -7,7 +7,7 @@ def __init__(self, index, city, street, house, apartment):
     self.apartment = apartment
 
 
-    def __str__(self):
+    def __str__ (self):
        return f"{self.index}, {self.city}, {self.street}, {self.house}, {self.apartment}"
     
     #Класс должен содержать в себе поля:
