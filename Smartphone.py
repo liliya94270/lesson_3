88d83972dc337265d18df07d8bfaa8dcae75478e
liliya94270 <liya.khabibullina.10@mail.ru>
2025-03-05 13:53:33 +0500

    Update Smartphone.py

--- Smartphone.py
+++ Smartphone.py
@@ -1,5 +1,5 @@
 class Smartphone: 
     def __init__(self, brand, model, number):
-        self.smartphone.br = brand
-        self.smartphone.md = model
-        self.smartphone.num = number 
+        self.brand= brand
+        self.model = model
+        self.number = number 
